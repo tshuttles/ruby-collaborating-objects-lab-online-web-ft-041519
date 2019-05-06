1fa5dab964433f227f40d3824df446c4db5c7682
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-06 21:36:35 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,40 +1,39 @@
 class Artist 
-  
   attr_accessor :name, :songs
-  
-  @@all = [] 
-  
-  def initialize(name)
-    @name = name 
-    @songs = []
-  end 
-  
-  def name
-    @name
-  end 
-  
-  def add_song(song)
+
+   @@all = []
+
+   def initialize(name)
+    @name = name
+    @songs =[]
+    self.save
+  end
+
+   def add_song(song)
+    song.artist = self
     @songs << song
+  end
+
+   def save
+    @@all << self
+  end
+
+   def self.all
+    @@all
+  end
+
+   def self.find_or_create_by_name(name)
+    if @@all.find { |artist| artist.name == name } == nil
+      Artist.new(name)      #self.class.new(name)
+    else
+      @@all.find { |artist| artist.name == name }
+    end
+  end
+
+   def print_songs
+    @songs.each do |song|
+      puts song.name
+    end
   end 
-  
-  def save
-    @@all << self  
-  end 
-  
-  def self.all 
-    @@all 
-  end 
-  
-  def self.find_or_create_by_name(name)
-    artist = self.all.find {|x| x.name == name} 
-    if !artist 
-      artist = self.new(name) 
-    end 
-    artist 
-  end 
-  
-  def print_songs
-    @songs.each {|song| puts song.name} 
-  end 
-  
+
 end 
