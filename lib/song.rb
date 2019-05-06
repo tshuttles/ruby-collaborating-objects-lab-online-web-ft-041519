f65374392ffe428f4f7f381fccb2e4d9af9557b4
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-06 21:35:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,6 +4,7 @@ class Song
   
   def initialize(name)
     @name = name 
+    @@all << self 
   end 
   
   def name
@@ -15,7 +16,12 @@ class Song
   end 
   
   def self.new_by_filename(filename)
-    
+    song_name = filename.split(" - ")[1]
+    artist_name = filename.split(" - ")[0]
+    song = Song.new(song_name)
+    song.artist = Artist.find_or_create_by_name(artist_name)
+    song.artist.add_song(song)
+    song
   end 
   
 end 
